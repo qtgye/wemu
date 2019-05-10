95d985ab16d38ac9dc7926b587fff619672fdb1d
Jace <buquia.jace@gmail.com>
2019-05-11 00:29:26 +0800

    Header scroll interaction

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -4,6 +4,9 @@ class Header extends Component {
 	constructor(element) {
 		super(element);
 
+		// Initial value
+		this.lastScroll = 0;
+
 		// Save references to children
 		this.toggle = element.querySelector('.header__nav-toggle');
 	}
@@ -14,11 +17,25 @@ class Header extends Component {
 		this.toggle.addEventListener('click', function(e) {
 			_this.handleToggleClick(e);
 		});
+
+		window.addEventListener('scroll', e => this.handleScroll());
 	}
 
 	handleToggleClick(e) {
 		e.preventDefault();
 		this.element.classList.toggle('header--expanded');
+	}
+
+	handleScroll() {
+		const currentScroll = window.scrollY;
+		const scrolled = currentScroll > 10;
+		const upScroll = this.lastScroll < currentScroll;
+
+		// Save new scroll value
+		this.lastScroll = currentScroll;
+
+		this.element.classList.toggle('header--opaque', scrolled);
+		this.element.classList.toggle('header--hidden', upScroll && scrolled);
 	}
 }
 
